3bf66b93fcdd71d3f428369b0d6bb1a968cdda43
Arta <gsumiartawan@gmail.com>
2025-01-28 15:33:02 +0700

    Show comment in form input and table component

--- src/pages/activity/components/activity-table.tsx
+++ src/pages/activity/components/activity-table.tsx
@@ -255,6 +255,19 @@ export const ActivityTable = ({
         },
       },
       {
+        id: 'comment',
+        accessorKey: 'comment',
+        enableSorting: false,
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Comment" />,
+        cell: ({ row }) => {
+          return (
+            <div className="ml-2 flex min-w-[50px] flex-col">
+              <span className="text-xs font-light">{row.getValue('comment')}</span>
+            </div>
+          );
+        },
+      },
+      {
         id: 'assetName',
         accessorKey: 'assetName',
         enableHiding: false,
@@ -378,7 +391,7 @@ export const ActivityTable = ({
         accountCurrency: false,
         assetName: false,
         currency: false,
-      }
+      },
     },
     state: {
       sorting,
